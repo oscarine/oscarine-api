a7adc8afd05670f288f00685c8ccedc868023bbb
Haider Ali <haider.lee23@gmail.com>
2019-09-15 19:03:55 +0530

    Add template for index page (#2)

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='Documentation')
