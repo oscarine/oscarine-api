7839f0276e4038067bad09bc6dfe2cba966b3926
Haider Ali <haider.lee23@gmail.com>
2019-09-16 00:16:05 +0530

    Add new fields for users (#6)

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 from app import db, login
 
 
@@ -8,6 +9,15 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    bio = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    first_name = db.Column(db.String(25))
+    last_name = db.Column(db.String(25))
+    phone_number = db.Column(db.String(15))
+    avatar_image = db.Column(db.String(120))
+    city = db.Column(db.String(30))
+    state = db.Column(db.String(30))
+    role = db.Column(db.String(10))
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,17 @@
 from flask import render_template
-from app import app
+from datetime import datetime
+from flask_login import current_user
+from app import app, db
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Documentation')
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
