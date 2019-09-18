bcc662529c2251b8724d174d65e7cc4999f5228a
Haider Ali <haider.lee23@gmail.com>
2019-09-19 01:52:03 +0530

    fix indentation

--- app/main/routes.py
+++ app/main/routes.py
@@ -8,7 +8,7 @@ from app.main import bp
 @bp.route('/')
 @bp.route('/index')
 def index():
-    return render_temdsewdewplate('index.html', title='Documentation')
+	return render_temdsewdewplate('index.html', title='Documentation')
 
 
 @bp.before_request
