adfaa10f7121e7e383175af9117736ba3d2ba057
Haider Ali <haider.lee23@gmail.com>
2019-09-19 01:47:30 +0530

    test syntax

--- app/main/routes.py
+++ app/main/routes.py
@@ -13,6 +13,6 @@ def index():
 
 @bp.before_request
 def before_request():
-    if current_user.is_authenticated:
-        current_user.last_seen = datetime.utcnow()dewdewdewddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-        db.session.commit()
+if current_user.is_authenticated:
+current_user.last_seen = datetime.utcnow()dewdewdewddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+db.session.commit()
