7c237afd5c526f3b6471b003402b8524f026e21f
Haider Ali <haider.lee23@gmail.com>
2019-10-07 22:27:56 +0530

    Improvide project structure with cookiecutter

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: flask db upgrade; gunicorn oscarine:app
+release: flask db upgrade
+web: gunicorn oscarine_api.app:create_app\(\) -b 0.0.0.0:$PORT -w 3
--- oscarine.py
+++
@@ -1,9 +0,0 @@
-from app import create_app, db
-from app.models import User
-
-app = create_app()
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User}
