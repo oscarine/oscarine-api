9439a08ceb0404d1d6d3b3687dec040ee737f8aa
Haider Ali <haider.lee23@gmail.com>
2020-05-11 00:04:24 +0530

    refactor: Update owner schema and remove username

--- app/db_models/owner.py
+++ app/db_models/owner.py
@@ -1,16 +1,17 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Boolean, DateTime
+from datetime import datetime
 
 from app.db.base_class import Base
 
 
 class Owner(Base):
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(64), index=True, unique=True)
     email = Column(String(120), index=True, unique=True)
     password_hash = Column(String())
-    first_name = Column(String(25))
-    last_name = Column(String(25))
+    name = Column(String(30))
     phone_number = Column(String(15))
     avatar_image = Column(String(120))
+    last_seen = Column(DateTime, default=datetime.utcnow)
     city = Column(String(30))
     state = Column(String(30))
+    email_verified = Column(Boolean, default=False)
