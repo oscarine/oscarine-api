89333a0c7a03288f4c4c1ed9b8587f66619b4a1e
Haider Ali <haider.lee23@gmail.com>
2020-05-15 23:42:00 +0530

    Add endpoints to GET shops list and shop by id

--- app/api/api_v1/endpoints/shops.py
+++ app/api/api_v1/endpoints/shops.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import PositiveInt
+from typing import List
 
 from sqlalchemy.orm import Session
 from app.api.utils.db import get_db
 from app.db_models.owner import Owner as DBOwnerModel
 from app.api.utils.owner_security import get_current_owner
 from app.models.shop import ShopDetails, ShopRegister
-from app.crud.shop import register_new_shop
+from app.crud.shop import register_new_shop, get_shop_by_id
 
 
 router = APIRouter()
@@ -20,3 +22,33 @@ async def register_shop(
 ):
     shop = register_new_shop(db, owner_id=current_owner.id, data=data)
     return shop
+
+
+@router.get("/shops", response_model=List[ShopDetails])
+async def list_of_shops(
+    *,
+    db: Session = Depends(get_db),
+    current_owner: DBOwnerModel = Depends(get_current_owner)
+):
+    if (shops: = current_owner.shops):
+        return shops
+    raise HTTPException(
+        status_code=404,
+        detail="This owner does not have any shops."
+    )
+
+
+@router.get("/shops/{shop_id}", response_model=ShopDetails)
+async def shop_by_id(
+    *,
+    shop_id: PositiveInt,
+    db: Session = Depends(get_db),
+    current_owner: DBOwnerModel = Depends(get_current_owner)
+):
+    shop = get_shop_by_id(db, shop_id=shop_id, owner_id=current_owner.id)
+    if shop:
+        return shop
+    raise HTTPException(
+        status_code=404,
+        detail="This owner does not have any shop with this id."
+    )
--- app/crud/shop.py
+++ app/crud/shop.py
@@ -19,3 +19,12 @@ def register_new_shop(db_session: Session, *, owner_id: int,
     db_session.commit()
     db_session.refresh(shop)
     return shop
+
+
+def get_shop_by_id(db_session: Session, *, shop_id: int,
+                   owner_id: int) -> Shop:
+    shop = db_session.query(Shop).filter_by(
+        id=shop_id, owner_id=owner_id).first()
+    if shop:
+        return shop
+    return None
