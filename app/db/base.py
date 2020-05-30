3705600bfa7395766671cf3701e68c8601352103
Haider Ali <haider.lee23@gmail.com>
2020-05-30 23:36:37 +0530

    feat: User addresses (#119)

--- app/db/base.py
+++ app/db/base.py
@@ -1,6 +1,7 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 from app.db.base_class import Base
+from app.db_models.address import Address
 from app.db_models.item import Item
 from app.db_models.owner import Owner
 from app.db_models.shop import Shop
