97d892c6622ac24365865347b78fc626fada6860
Haider Ali <haider.lee23@gmail.com>
2020-06-02 22:35:34 +0530

    fix: send new OTP when user updates email

--- app/api/api_v1/endpoints/users.py
+++ app/api/api_v1/endpoints/users.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta
-from secrets import SystemRandom
 
 from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.api.utils.db import get_db
+from app.api.utils.otp import generate_random_otp
 from app.api.utils.security import get_current_user
 from app.core import config
 from app.core.email import send_email_verify_otp
@@ -37,7 +37,7 @@ async def register_user(
         raise HTTPException(
             status_code=400, detail="The user with this email already exists.",
         )
-    otp = SystemRandom().randint(10000, 99999)
+    otp = generate_random_otp()
     user = create_user(db, user_in=data, otp=otp)
     background_tasks.add_task(send_email_verify_otp, data.email, otp)
     return user
@@ -74,6 +74,20 @@ async def update_user(
     db: Session = Depends(get_db),
     data: UserUpdate,
     current_user: DBUser = Depends(get_current_user),
+    background_tasks: BackgroundTasks,
 ):
-    user = update_user_info(db, data=data, user=current_user)
-    return user
+    """If `email` is found in data, email will no longer be verified
+       and new OTP as well as OTP datetime will be generated.
+    """
+    otp = None
+    if data.email:
+        if get_by_email(db, email=data.email):
+            raise HTTPException(
+                status_code=409, detail="The user with this email already exists."
+            )
+        otp = generate_random_otp()
+    if user := update_user_info(db, data=data, user=current_user, otp=otp):
+        if otp and user.email:
+            background_tasks.add_task(send_email_verify_otp, user.email, otp)
+        return user
+    raise HTTPException(status_code=400, detail="Cannot update user info.")
--- app/crud/user.py
+++ app/crud/user.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional
 
 from fastapi import HTTPException
@@ -5,6 +6,7 @@ from fastapi.encoders import jsonable_encoder
 from pydantic import EmailStr
 from sqlalchemy.orm import Session
 
+from app.api.utils.otp import generate_random_otp
 from app.core.security import get_password_hash, verify_password
 from app.db_models.user import User
 from app.models.user import UserCreate, UserUpdate
@@ -44,16 +46,21 @@ def authenticate(
     return user
 
 
-def update_user_info(db_session: Session, *, user: User, data: UserUpdate) -> User:
+def update_user_info(
+    db_session: Session, *, user: User, data: UserUpdate, otp: int = None
+) -> User:
+    """If `otp` is sent in the argument:
+        `user.otp = otp`
+        `user.email_verified = False`
+        `user.otp_created_at = datetime.utcnow()`
+    """
     data = jsonable_encoder(data, exclude_none=True)
-    if "email" in data:
-        user_by_email = get_by_email(db_session, email=data["email"])
-        if user_by_email:
-            raise HTTPException(
-                status_code=422, detail="The user with this email already exists."
-            )
     for field in data:
         setattr(user, field, data[field])
+    if otp:
+        user.otp = otp
+        user.email_verified = False
+        user.otp_created_at = datetime.utcnow()
     db_session.add(user)
     db_session.commit()
     db_session.refresh(user)
