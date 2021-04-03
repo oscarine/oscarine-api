748caf03045faf34a94cd8c68fbe0a4a57ec6f5b
Haider Ali <haider.lee23@gmail.com>
2021-04-04 03:18:21 +0530

    Add env variables in testing script

--- tests-start.sh
+++ tests-start.sh
@@ -3,6 +3,9 @@
 set -e
 
 export TESTING=True
+export PROJECT_NAME=oscarine-api
+export BACKEND_CORS_ORIGINS=http://localhost, http://localhost:4200, http://localhost:3000, http://localhost:8080, http://localhost:8000
+export SECRET_KEY=my-secret-key-for-testing
 
 python app/tests_pre_start.py
 
@@ -10,3 +13,6 @@ bash ./scripts/test.sh "$@"
 
 echo "I am done testing!"
 unset TESTING
+unset PROJECT_NAME
+unset BACKEND_CORS_ORIGINS
+unset SECRET_KEY
