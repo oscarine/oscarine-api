3af81062c547f3639dea02f67f8519a9f8f028d1
Haider Ali <haider.lee23@gmail.com>
2021-05-23 19:10:02 +0530

    Remove unused variables

--- app/tests/api/api_v1/test_shops.py
+++ app/tests/api/api_v1/test_shops.py
@@ -9,11 +9,10 @@ def test_get_shops_for_user_lat_long(client: TestClient, db: Session):
     longitude = 78.0705664
     latitude = 27.8983082
     radius = 1000
-    shop = create_shop_with_longitude_latitude_radius(
+    create_shop_with_longitude_latitude_radius(
         db=db, longitude=longitude, latitude=latitude, radius_metres=radius
     )
     r = client.get(
         f"{config.API_V1_STR}/shops-list?longitude={longitude}&latitude={latitude}"
     )
-    response_json = r.json()
     assert r.status_code == 200
